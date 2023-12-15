53583ddcdf39fd3571d26ff2a347b28189b5b1ff
Mark <wernsdorfer+github@gmail.com>
2023-12-15 17:03:55 +0100

    html text indexing

--- src/controller.py
+++ src/controller.py
@@ -1,6 +1,7 @@
 import lingua
 import nltk
 
+from src.agents.extraction import AgentExtraction
 from src.dataobjects import ViewCallbacks
 from src.model.model import Model
 from src.tools.bookmarklet import get_bookmarklet_template
@@ -11,30 +12,20 @@ class Controller:
     def __init__(self, config: dict[str, any]) -> None:
         bookmarklet_template = get_bookmarklet_template()
 
-        nltk.download('punkt')
-        detector = lingua.LanguageDetectorBuilder.from_all_languages()
-        self.detector_built = detector.build()
+        config_agent = config.pop("agent_interface")
+        self.extractor_agent = AgentExtraction(config_agent)
 
         config_databases = config.pop("redis")
         self.model = Model(config_databases)
         self.view = View(bookmarklet_template)
 
-        self.agent_config = config.pop("agent_interface")
         view_callbacks = ViewCallbacks(
             self.model.dummy_function,
-            self.get_agent_config,
-            self.detect_language
+            self.get_extractor_agent,
         )
 
         self.view.set_callbacks(view_callbacks)
         self.view.setup_routes()
 
-    def get_agent_config(self) -> dict[str, any]:
-        return dict(self.agent_config)
-
-    def detect_language(self, text: str) -> str:
-        language = self.detector_built.detect_language_of(text)
-        if language is None:
-            return "en"
-
-        return language.iso_code_639_1.name.lower()
+    def get_extractor_agent(self) -> AgentExtraction:
+        return self.extractor_agent
--- src/view/view.py
+++ src/view/view.py
@@ -1,12 +1,16 @@
 # coding=utf-8
+from loguru import logger
+from lxml import etree, html
 from nicegui import ui, Client, app
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, Response
 import validators
 
+from experiments.navi_str import XpathSlice
+from experiments.text_extraction import readability_lxml_extract, readability_lxml_extract_from_html
 from src.dataobjects import ViewCallbacks, Source
 from src.view.landing_page import LandingPage
 from src.view.processing_page import ProcessingPage
@@ -45,12 +49,49 @@ class View:
         target_url = self._get_target_url()
         ui.open(target_url)
 
-    def _update_html_body(self, body: str) -> str:
-        # use bs4 to enclose all text in <p> tags in <span class="doppelchecked"> tags
-        soup = BeautifulSoup(body, 'html.parser')
-        for p in soup.find_all('p'):
-            p.wrap(soup.new_tag("span", **{"class": "doppelchecked"}))
-        return str(soup)
+    def _highlight_slice(self, html_text: str, xslice: XpathSlice) -> str:
+        tree = html.fromstring(html_text)
+        for each_xpath in xslice.xpaths:
+            text_nodes = tree.xpath(each_xpath)
+            for each_node in text_nodes:
+                # Parse the node text with BeautifulSoup
+                soup = BeautifulSoup(each_node.text, 'html.parser')
+                # Find all span elements
+                spans = soup.find_all('span')
+                # Replace each span with its own text content
+                for span in spans:
+                    span.replace_with(span.text)
+                # Get the modified text
+                modified_text = str(soup)
+                # Replace xslice.text in the modified text with a span-wrapped version of xslice.text
+                new_content = modified_text.replace(xslice.text, f'<span class="doppelchecked">{xslice.text}</span>')
+                parent = each_node.getparent()
+                parent.replace(each_node, etree.fromstring(f"<div>{new_content}</div>"))
+
+        return html.tostring(tree).decode("utf-8")
+
+    def _highlight_text_section(self, soup: BeautifulSoup, marked_text: str) -> BeautifulSoup:
+        for text_node in soup.find_all(text=True):
+            if marked_text in text_node:
+                new_content = list()
+                parts = text_node.split(marked_text)
+
+                for i, part in enumerate(parts):
+                    new_content.append(NavigableString(part))
+                    if i < len(parts) - 1:  # Don't add span after the last part
+                        span_tag = soup.new_tag("span", **{"class": "doppelchecked"})
+                        span_tag.string = marked_text
+                        new_content.append(span_tag)
+
+                # Replace the text node with the new content
+                current = text_node
+                for content in new_content:
+                    current.insert_after(content)
+                    current = content
+
+                text_node.extract()
+
+        return soup
 
     def setup_routes(self) -> None:
         @app.post("/pass_source/")
@@ -61,9 +102,61 @@ class View:
 
         @app.post("/update_body/")
         async def pass_source(source: Source) -> Response:
-            body = source.body
-            new_body = self._update_html_body(body)
-            return JSONResponse(content={"body": new_body})
+            html_text = source.html
+            soup = BeautifulSoup(html_text, "html.parser")
+
+            # process
+            extractor_agent = self.callbacks.get_extractor_agent()
+
+            if source.selected_text is None:
+                # take website
+                main_content = html_text
+                # main_content = readability_lxml_extract_from_html(body)
+                sourced_statements = await extractor_agent.extract_statements_from_html(main_content)
+
+                for each_indices, each_statement in sourced_statements:
+                    logger.info(f"Statement: {each_statement}")
+                    logger.info(f"Source: {each_indices}")
+                    for each_index in each_indices:
+                        html_text = self._highlight_slice(html_text, each_index)
+
+                soup = BeautifulSoup(html_text, "html.parser")
+                statements = [each_statement for _, each_statement in sourced_statements]
+
+            else:
+                # take selection (get html of selection?)
+                text = source.selected_text
+                sourced_statements = await extractor_agent.extract_statements_from_text(text)
+
+                for each_statement, each_source in sourced_statements:
+                    logger.info(f"Statement: {each_statement}")
+                    logger.info(f"Source: {each_source}")
+                    soup = self._highlight_text_section(soup, each_source)
+
+                statements = [each_statement for each_statement, _ in sourced_statements]
+
+            # add sidebar
+            elements_statements = "\n".join(
+                f'<li style="margin-bottom: 1em;">{each_statement}</li>'
+                for each_statement in statements
+            )
+            sidebar_html = f"""
+<div class="doppelcheck-sidebar" style="width: 200px; height: 100%; position: fixed; top: 0; right: 0; color: black; background: #f0f0f0; padding: 10px; z-index: 10000;">
+    <div class="doppelcheck-sidebar-content">
+        <h1>Side bar</h1>
+        <ul>
+            {elements_statements}
+        </ul>
+    </div>
+</div>
+            """
+
+            sidebar = BeautifulSoup(sidebar_html, "html.parser")
+            soup.body.insert(0, sidebar)
+
+            # body_html = body_soup.prettify()
+            body_html = str(soup.body)
+            return JSONResponse(content={"body": body_html})
 
         @ui.page("/")
         async def index_page(client: Client) -> None:
