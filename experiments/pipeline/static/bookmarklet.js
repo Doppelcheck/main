335bded35847439050b68d99d6205505354488e7
mark <wernsdorfer@gmail.com>
2024-01-26 07:32:15 +0100

    prototype for cross domain communication

--- experiments/pipeline/static/bookmarklet.js
+++ experiments/pipeline/static/bookmarklet.js
@@ -1,5 +1,103 @@
 const address = "localhost:8000";
 
+const WebStorageAPIStuff = {
+    hasAccess: false,
+
+    updateCookiesOutput() {
+      console.log("Reading local storage:", localStorage);
+    },
+
+    // This function is called by the refreshCookiesButton
+    // It either already has access - in which case it can simply access the cookies
+    // Or it doesn't have access because it was waiting for a prompt
+    // We could just gate this on `hasAccess`
+    async refreshCookies() {
+      if (!WebStorageAPIStuff.hasAccess) {
+        console.log(
+          "Don't have access. Trying again within this click handler, in case it needed a prompt."
+        );
+        try {
+          // This should now work if it was waiting a prompt
+          await document.requestStorageAccess();
+          WebStorageAPIStuff.hasAccess = true;  // Can assume this was true is above did not reject
+          console.log("Have access now thanks to prompt");
+        } catch (err) {
+          console.log("requestStorageAccess Error:", err);
+        }
+
+        WebStorageAPIStuff.hasAccess = await document.hasStorageAccess();
+        console.log("Updated hasAccess:", WebStorageAPIStuff.hasAccess);
+      }
+      WebStorageAPIStuff.updateCookiesOutput();
+    },
+
+    async hasCookieAccess() {
+      // Check if Storage Access API is supported
+      if (!document.requestStorageAccess) {
+        // Storage Access API is not supported so best we can do is
+        // hope it's an older browser that doesn't block 3P cookies
+        console.log("Storage Acccess API not supported. Assume we have access.")
+        return true;
+      }
+
+      // Check if access has already been granted
+      if (await document.hasStorageAccess()) {
+        console.log("Cookie access already granted");
+        return true;
+      }
+
+      // Check the storage-access permission
+      // Wrap this in a try/catch for browsers that support the
+      // Storage Access API but not this permission check
+      // (e.g. Safari and older versions of Firefox)
+      try {
+        const permission = await navigator.permissions.query({
+          name: "storage-access",
+        });
+        console.log("permissions:", permission);
+
+        if (permission.state === "granted") {
+          // Can just call requestStorageAccess() without a
+          // user interaction and it will resolve automatically.
+          try {
+            console.log("Cookie access allowed. Calling requestStorageAccess()");
+            await document.requestStorageAccess();
+            console.log("Cookie access granted");
+            return true;
+          } catch (error) {
+            // This shouldn't really fail if access is granted
+            return false;
+          }
+        } else if (permission.state === "prompt") {
+          // Need to call requestStorageAccess() after a user interaction
+          // Can't do anything further here, so handle this in the click handler
+          console.log("Cookie access requires a prompt");
+          return false;
+        } else if (permission.state === "denied") {
+          // Currently not used. See:
+          // https://github.com/privacycg/storage-access/issues/149
+          console.log("Cookie access denied");
+          return false;
+        }
+      } catch (error) {
+        // storage-access permission not supported. Assume false.
+          console.log("storage-access permission not supported. Assume no access.");
+        return false;
+      }
+
+      // By default return false, though should really be caught by one of above.
+      return false;
+    },
+
+    // This function runs as page loads to try to give access initially
+    // This can make the click handler quicker as it doesn't need to
+    // await the access request if it's already happened.
+    async handleCookieAccessInit() {
+      WebStorageAPIStuff.hasAccess = await WebStorageAPIStuff.hasCookieAccess();
+      WebStorageAPIStuff.updateCookiesOutput();
+    }
+}
+
 const IFrameCommunication = {
                 // Create an IFrame and append it to the body
     initIframe() {
@@ -26,7 +124,10 @@ const IFrameCommunication = {
             }
         });
     },
-    setHelper(key, value) {
+    async setHelper(key, value) {
+        await WebStorageAPIStuff.refreshCookies();
+        await WebStorageAPIStuff.handleCookieAccessInit();
+
         const iframe = document.getElementById('doppelcheck-iframe');
         const message = {type: 'save', key: key, value: value };
         console.log("BOOKMARKLET: Sending data to helper", message);
@@ -35,7 +136,10 @@ const IFrameCommunication = {
             `https://${address}`
         );
     },
-    getHelper(key) {
+    async getHelper(key) {
+        await WebStorageAPIStuff.refreshCookies();
+        await WebStorageAPIStuff.handleCookieAccessInit();
+
         const iframe = document.getElementById('doppelcheck-iframe');
         const message = {type: 'load', key: key };
         console.log("BOOKMARKLET: Requesting data from helper ", message);
