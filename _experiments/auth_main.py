e6ad9a0d8564388fcce4532d984543a74d133312
Mark <wernsdorfer+github@gmail.com>
2024-02-15 20:36:43 +0100

    started authentication

---
+++ _experiments/auth_main.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+"""This is just a simple authentication example.
+
+Please see the `OAuth2 example at FastAPI <https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/>`_  or
+use the great `Authlib package <https://docs.authlib.org/en/v0.13/client/starlette.html#using-fastapi>`_ to implement a classing real authentication system.
+Here we just demonstrate the NiceGUI integration.
+"""
+from typing import Optional
+
+from fastapi import Request
+from fastapi.responses import RedirectResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from nicegui import Client, app, ui
+
+# in reality users passwords would obviously need to be hashed
+passwords = {'user1': 'pass1', 'user2': 'pass2'}
+
+unrestricted_page_routes = {'/login', "/stuff"}
+
+
+class AuthMiddleware(BaseHTTPMiddleware):
+    """This middleware restricts access to all NiceGUI pages.
+
+    It redirects the user to the login page if they are not authenticated.
+    """
+
+    async def dispatch(self, request: Request, call_next):
+        if not app.storage.user.get('authenticated', False):
+            if request.url.path in Client.page_routes.values() and request.url.path not in unrestricted_page_routes:
+                app.storage.user['referrer_path'] = request.url.path  # remember where the user wanted to go
+                return RedirectResponse('/login')
+        return await call_next(request)
+
+
+app.add_middleware(AuthMiddleware)
+
+
+@ui.page('/')
+def main_page() -> None:
+    with ui.column().classes('absolute-center items-center'):
+        ui.label(f'Hello {app.storage.user["username"]}!').classes('text-2xl')
+        ui.button(on_click=lambda: (app.storage.user.clear(), ui.open('/login')), icon='logout').props('outline round')
+
+
+@ui.page('/subpage')
+def test_page() -> None:
+    ui.label('This is a sub page.')
+
+
+@ui.page('/stuff/{stuff_id}')
+def test_page(stuff_id: str) -> None:
+    ui.label(f'This is a page with stuff. ID is {stuff_id}.')
+
+
+@ui.page('/login')
+def login() -> Optional[RedirectResponse]:
+    def try_login() -> None:  # local function to avoid passing username and password as arguments
+        if passwords.get(username.value) == password.value:
+            app.storage.user.update({'username': username.value, 'authenticated': True})
+            ui.open(app.storage.user.get('referrer_path', '/'))  # go back to where the user wanted to go
+        else:
+            ui.notify('Wrong username or password', color='negative')
+
+    if app.storage.user.get('authenticated', False):
+        return RedirectResponse('/')
+
+    with ui.card().classes('absolute-center'):
+        username = ui.input('Username').on('keydown.enter', try_login)
+        password = ui.input('Password', password=True, password_toggle_button=True).on('keydown.enter', try_login)
+        ui.button('Log in', on_click=try_login)
+
+    return None
+
+
+"""
+# existing
+/           accessible      landing page, installation  like /stuff
+/config     accessible      user configuration          like /stuff
+/admin      restricted      system configuration        like / and /subpage
+
+# todo
+/login      accessible      login page                  like /login
+"""
+
+ui.run(storage_secret='THIS_NEEDS_TO_BE_CHANGED')
