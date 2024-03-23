362bb5076f6ba71f923726fc32990573c3d8ec0b
mark <wernsdorfer@gmail.com>
2024-03-23 06:45:22 +0100

    draggable experiment

--- _experiments/order_list/main.py
+++ _experiments/order_list/main.py
@@ -4,7 +4,7 @@ from draganddrop import DraggableRow, Item
 from nicegui import ui
 
 
-def main() -> None:
+def _main() -> None:
     with DraggableRow() as row:
         row.add_column('Next', [
             Item('Simplify Layouting'),
@@ -19,6 +19,25 @@ def main() -> None:
             Item('Publish as Open Source'),
             Item('Release Native-Mode')
         ])
+
+    ui.run()
+
+
+def main() -> None:
+    with ui.column() as draggable_list:
+        draggable_list.classes('bg-grey-3 p-4 rounded')
+
+        draggable_list.on('drop', lambda event: print('drop', event))
+
+        for each_text in ["Next", "Doing", "Done"]:
+            with ui.card() as each_card:
+                each_card.classes('w-full p-4 cursor-move')
+                each_card.props('draggable')
+
+                ui.label(each_text)
+
+            each_separator = ui.separator()
+            each_separator.on('drop', lambda event: print('drop', event))
 
     ui.run()
 
