110dbb9c949844dfcfaf0b02d55138007ec4f960
mark <wernsdorfer@gmail.com>
2024-03-27 13:02:29 +0100

    moved data sources to sourcefinder

--- configuration/config_03_retrieval.py
+++ configuration/config_03_retrieval.py
@@ -8,6 +8,36 @@ def get_section_sourcefinder(user_id: str | None) -> None:
     llm_interfaces = ConfigModel.get_llm_interfaces(user_id)
     data_interfaces = ConfigModel.get_data_interfaces(user_id)
 
+    def _update_data() -> None:
+        nonlocal data_interfaces
+        data_interfaces = ConfigModel.get_data_interfaces(user_id)
+        data_select.options = [each_name for each_name in data_interfaces]
+        data_select.update()
+
+    def toggle_chips() -> None:
+        if len(data_select.value) < 2:
+            data_select.props(remove='use-chips')
+        else:
+            data_select.props(add='use-chips')
+
+    default_data = ConfigModel.get_selected_data_interfaces(user_id)
+    with ui.select(
+            options=[each_name for each_name in data_interfaces],
+            label="Data interfaces", value=[each_data.name for each_data in default_data], multiple=True,
+            on_change=toggle_chips
+    ) as data_select, Store(data_select, lambda names: ConfigModel.set_selected_data_interfaces(user_id, names)):
+        data_select.classes('w-full').on("click", _update_data)
+        if (user_id is not None) and not AccessModel.get_select_data():
+            data_select.disable()
+            ui.tooltip("User does not have access to change this setting.")
+
+    toggle_chips()
+    if user_id is None:
+        with ui.checkbox(
+                text="User access", value=AccessModel.get_select_data()
+        ) as checkbox, Store(checkbox, AccessModel.set_select_data):
+            pass
+
     def _update_llms() -> None:
         nonlocal llm_interfaces
         llm_interfaces = ConfigModel.get_llm_interfaces(user_id)
