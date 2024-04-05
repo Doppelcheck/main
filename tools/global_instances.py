9d939c2f4eaaa057d886153e1024f5ecd21c05c8
mark <wernsdorfer@gmail.com>
2024-04-05 11:55:18 +0200

    added wikipedia interface

--- tools/global_instances.py
+++ tools/global_instances.py
@@ -1,4 +1,5 @@
 import httpx
+import wikipedia
 from lingua import lingua
 
 from tools.browser import PlaywrightBrowser
@@ -8,3 +9,5 @@ HTTPX_SESSION = httpx.AsyncClient()
 
 detector = lingua.LanguageDetectorBuilder.from_all_languages()
 DETECTOR_BUILT = detector.build()
+
+WIKIPEDIA_LANGUAGES = wikipedia.languages()
