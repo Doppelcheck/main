6d8bf4a468030136a4905beaf2fdb1c081df11da
mark <wernsdorfer@gmail.com>
2024-04-10 22:15:29 +0200

    trying function calling with ollama

--- _experiments/local_llama.py
+++ _experiments/local_llama.py
@@ -291,14 +291,16 @@ async def main() -> None:
 
     text_lines = list(get_text_lines(document, line_length=30))
 
-    # client = AsyncClient(host="http://localhost:8800")
-    client = instructor.from_openai(
+    client = AsyncClient(host="http://localhost:8800")
+    _client = instructor.from_openai(
         OpenAI(
             base_url="http://localhost:8800",
             api_key="ollama"
         ),
         mode=instructor.Mode.JSON
     )
+
+    client = instructor.patch(client, mode=instructor.Mode.JSON)
 
     interface = SummarizationInterface(text_lines, 3, line_window=10)
     while True:
